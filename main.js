c3411b07bef688aee79db1bbd3bd49b00f0911de
BartetoKox <90682115+BartetoKox@users.noreply.github.com>
2021-11-30 10:25:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+let library = [];
+
+const temp = document.querySelector('.book');
+const bookshelf = document.querySelector('#bookshelf');
+let idBook = library.length;
+
+function Book(title, author) {
+  this.id = idBook;
+  this.title = title;
+  this.author = author;
+ ;
+
+  idBook += 1;
+}
+
+function ReloadLibrary() {
+  library = JSON.parse(localStorage.library);
+
+  bookshelf.innerHTML = '';
+  bookshelf.appendChild(temp);
+
+  for (let i = 0; i < library.length; i += 1) {
+    DisplayBook(library[i]);
+  }
+}
+
+function SaveBook(title, author) {
+  const book = new Book(title, author);
+  if (!Array.isArray(library)) {
+    library = [];
+  }
+  library.push(book);
+
+  localStorage.library = JSON.stringify(library);
+
+  ReloadLibrary();
+}
+function AddBook() {
+  event.preventDefault();
+
+  const formAddBook = document.forms.AddBook;
+  const bookData = new FormData(formAddBook);
+
+  const bookTitle = bookData.get('title');
+  const bookAuthor = bookData.get('author');
+
+
+  formAddBook.reset();
+
+  SaveBook(bookTitle, bookAuthor);
+}
+
+function DeleteBook(id) {
+  library = library.filter((book) => book.id !== id);
+
+  localStorage.library = JSON.stringify(library);
+
+  ReloadLibrary();
+}
+
+function DisplayBook(book) {
+  const clon = temp.content.cloneNode(true);
+  clon.querySelectorAll('p')[0].innerHTML = 'Nazwa książki: '+book.title;
+  clon.querySelectorAll('p')[1].innerHTML = 'Autor ksiązki: '+book.author;
+
+
+  clon.querySelector('button').addEventListener('click', () => { DeleteBook(book.id); });
+
+  bookshelf.appendChild(clon);
+}
+
+ReloadLibrary();
